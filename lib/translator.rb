548578b46d5bbda5630ccb0ed016e21c8ae12b8e
Joshua Kim <jykim92@gmail.com>
2020-04-11 15:23:16 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -32,7 +32,7 @@ def get_english_meaning(file_path = './lib/emoticons.yml', japanese_emoticon)
   result = load_library(file_path)
   test = result.keys.find do |emoticon|
     result[emoticon][:japanese] == japanese_emoticon
-    binding.pry
+
   end
   test ? test : "Sorry, that emoticon was not found"
 
